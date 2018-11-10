24364bdbd46071aff075fe55153b146f8daa2971
Yuliia Tesliuk <ytesliuk.gmail.com>
2018-11-10 13:22:59 +0200

    state pattern

---
+++ src/main/java/patterns/state/MushroomerState.java
@@ -0,0 +1,12 @@
+package patterns.state;
+
+/**
+ * Created by Yuliia Tesliuk on 11/10/2018
+ */
+public class MushroomerState implements State {
+
+    public String action() {
+        return "picking mushrooms.";
+    }
+}
+
---
+++ src/main/java/patterns/state/State.java
@@ -0,0 +1,9 @@
+package patterns.state;
+
+/**
+ * Created by Yuliia Tesliuk on 11/10/2018
+ */
+public interface State {
+
+    String action();
+}
