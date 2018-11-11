78e18b445e14ec669646e4a3dcf02df33fb09a77
Yuliia Tesliuk <ytesliuk.gmail.com>
2018-11-11 21:28:54 +0200

    Builder pattern

---
+++ src/main/java/patterns/builder/TourCompositionBuilder.java
@@ -0,0 +1,47 @@
+package patterns.builder;
+
+/**
+ * Created by Yuliia Tesliuk on 11/11/2018
+ */
+public class TourCompositionBuilder {
+    private TourComposition composition;
+    private boolean transferToAirport;
+    private boolean transferFromAirport;
+    private boolean flightTicketTo;
+    private boolean flightTicketFrom;
+    private boolean hotelReservation;
+    private boolean insurance;
+
+    public TourCompositionBuilder(){
+        composition = new TourComposition();
+    }
+
+    public TourCompositionBuilder setTransferToAirport(boolean bool){
+        composition.setTransferToAirport(bool);
+        return this;
+    }
+    public TourCompositionBuilder setTransferFromAirport(boolean bool){
+        composition.setTransferFromAirport(bool);
+        return this;
+    }
+    public TourCompositionBuilder setFlightTicketTo(boolean bool){
+        composition.setFlightTicketTo(bool);
+        return this;
+    }
+    public TourCompositionBuilder setFlightTicketFrom(boolean bool){
+        composition.setFlightTicketFrom(bool);
+        return this;
+    }
+    public TourCompositionBuilder setHotelReservation(boolean bool){
+        composition.setHotelReservation(bool);
+        return this;
+    }
+    public TourCompositionBuilder setInsurance(boolean bool){
+        composition.setInsurance(bool);
+        return this;
+    }
+    public TourComposition build(){
+        return composition;
+    }
+
+}
