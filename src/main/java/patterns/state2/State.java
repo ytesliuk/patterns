df179b91e228a4ed40864066e20553e32c799b9f
Yuliia Tesliuk <ytesliuk.gmail.com>
2018-11-13 00:04:05 +0200

    state pattern realization. Plane case

---
+++ src/main/java/patterns/state2/State.java
@@ -0,0 +1,46 @@
+package patterns.state2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by Yuliia Tesliuk on 11/12/2018
+ */
+public abstract class State {
+    private Plane plane;
+    private Scanner reader = new Scanner (System.in);
+    protected Map<Armament,Integer> planeArmament;
+
+
+    protected State(Plane plane) {
+        this.plane = plane;
+        planeArmament = getPlane().getArmament();
+    }
+
+
+    abstract void fly();
+    abstract void fire(Armament armament);
+    abstract void ground();
+    abstract void setTheWeapon(Armament armament);
+    abstract void replenishAmmunition(Armament armament, int amount);
+
+    protected Plane getPlane() {
+        return plane;
+    }
+
+    protected Scanner getReader() {
+        return reader;
+    }
+
+    protected boolean question(String string){
+        while(true){
+            System.out.println(string + "(Y/N)");
+            String answer = getReader().nextLine();
+            if(answer.equals("Y")){
+                return true;
+            } else if (answer.equals("N")) {
+                return false;
+            }
+        }
+    }
+}
